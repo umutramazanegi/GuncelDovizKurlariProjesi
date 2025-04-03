339af151256b5a64ff23c1bb5e5f3fd43bb85773
Umut Ramazan Eği <78850577+umutramazanegi@users.noreply.github.com>
2025-04-03 16:23:55 +0300

    RapidApi

---
+++ GuncelDovizKurlariProjesi/GuncelDovizKurlariProjesi/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace GuncelDovizKurlariProjesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPound = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rdbPound = new System.Windows.Forms.RadioButton();
+            this.rdbEuro = new System.Windows.Forms.RadioButton();
+            this.rdbDollar = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.lblEuro = new System.Windows.Forms.Label();
+            this.lblDollar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPound
+            // 
+            this.lblPound.AutoSize = true;
+            this.lblPound.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPound.Location = new System.Drawing.Point(470, 27);
+            this.lblPound.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPound.Name = "lblPound";
+            this.lblPound.Size = new System.Drawing.Size(64, 25);
+            this.lblPound.TabIndex = 23;
+            this.lblPound.Text = "label3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(17, 209);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 25);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Ödenecek Tutar:";
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTotalPrice.Location = new System.Drawing.Point(193, 209);
+            this.txtTotalPrice.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.Size = new System.Drawing.Size(194, 28);
+            this.txtTotalPrice.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(207, 245);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 38);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "İşlemi Yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rdbPound
+            // 
+            this.rdbPound.AutoSize = true;
+            this.rdbPound.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbPound.Location = new System.Drawing.Point(279, 129);
+            this.rdbPound.Margin = new System.Windows.Forms.Padding(2);
+            this.rdbPound.Name = "rdbPound";
+            this.rdbPound.Size = new System.Drawing.Size(83, 28);
+            this.rdbPound.TabIndex = 19;
+            this.rdbPound.TabStop = true;
+            this.rdbPound.Text = "Sterlin";
+            this.rdbPound.UseVisualStyleBackColor = true;
+            // 
+            // rdbEuro
+            // 
+            this.rdbEuro.AutoSize = true;
+            this.rdbEuro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbEuro.Location = new System.Drawing.Point(165, 129);
+            this.rdbEuro.Margin = new System.Windows.Forms.Padding(2);
+            this.rdbEuro.Name = "rdbEuro";
+            this.rdbEuro.Size = new System.Drawing.Size(72, 28);
+            this.rdbEuro.TabIndex = 18;
+            this.rdbEuro.TabStop = true;
+            this.rdbEuro.Text = "Euro";
+            this.rdbEuro.UseVisualStyleBackColor = true;
+            // 
+            // rdbDollar
+            // 
+            this.rdbDollar.AutoSize = true;
+            this.rdbDollar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbDollar.Location = new System.Drawing.Point(47, 129);
+            this.rdbDollar.Margin = new System.Windows.Forms.Padding(2);
+            this.rdbDollar.Name = "rdbDollar";
+            this.rdbDollar.Size = new System.Drawing.Size(75, 28);
+            this.rdbDollar.TabIndex = 17;
+            this.rdbDollar.TabStop = true;
+            this.rdbDollar.Text = "Dolar";
+            this.rdbDollar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(65, 171);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 25);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Birim Tutar:";
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUnitPrice.Location = new System.Drawing.Point(193, 171);
+            this.txtUnitPrice.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(194, 28);
+            this.txtUnitPrice.TabIndex = 15;
+            // 
+            // lblEuro
+            // 
+            this.lblEuro.AutoSize = true;
+            this.lblEuro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEuro.Location = new System.Drawing.Point(274, 27);
+            this.lblEuro.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEuro.Name = "lblEuro";
+            this.lblEuro.Size = new System.Drawing.Size(64, 25);
+            this.lblEuro.TabIndex = 14;
+            this.lblEuro.Text = "label3";
+            // 
+            // lblDollar
+            // 
+            this.lblDollar.AutoSize = true;
+            this.lblDollar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDollar.Location = new System.Drawing.Point(38, 27);
+            this.lblDollar.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDollar.Name = "lblDollar";
+            this.lblDollar.Size = new System.Drawing.Size(64, 25);
+            this.lblDollar.TabIndex = 13;
+            this.lblDollar.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 291);
+            this.Controls.Add(this.lblPound);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTotalPrice);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rdbPound);
+            this.Controls.Add(this.rdbEuro);
+            this.Controls.Add(this.rdbDollar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.lblEuro);
+            this.Controls.Add(this.lblDollar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPound;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTotalPrice;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rdbPound;
+        private System.Windows.Forms.RadioButton rdbEuro;
+        private System.Windows.Forms.RadioButton rdbDollar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.Label lblEuro;
+        private System.Windows.Forms.Label lblDollar;
+    }
+}
+
---
+++ GuncelDovizKurlariProjesi/GuncelDovizKurlariProjesi/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System; // Temel .NET sınıflarını kullanmak için.
+using System.Collections.Generic; // Generic koleksiyonları kullanmak için.
+using System.ComponentModel; // WinForms tasarımcısı için gerekli.
+using System.Data; // Veri yapılarını kullanmak için (kullanılmıyor).
+using System.Drawing; // Grafik işlemleri için (WinForms).
+using System.Linq; // LINQ sorguları için (kullanılmıyor).
+using System.Text; // Metin işlemleri için.
+using System.Threading.Tasks; // Asenkron işlemler (async/await) için.
+using System.Windows.Forms; // Windows Forms kontrolleri için.
+using System.Net.Http; // HTTP istekleri yapmak için (API).
+using Newtonsoft.Json.Linq; // JSON verilerini işlemek için (Newtonsoft.Json).
+
+namespace GuncelDovizKurlariProjesi // Projenin ad alanı.
+{
+    public partial class Form1 : Form // Ana form sınıfı, Form'dan türetilmiş.
+    {
+        public Form1() // Formun yapıcı metodu (constructor).
+        {
+            InitializeComponent(); // Form tasarımındaki kontrolleri yükler.
+        }
+
+        // Döviz kurlarını saklamak için sınıf seviyesinde değişkenler.
+        decimal dollar = 0; // Dolar kuru (TRY).
+        decimal euro = 0;   // Euro kuru (TRY).
+        decimal pound = 0;  // Sterlin kuru (TRY).
+
+        // Form yüklendiğinde çalışan olay işleyici (asenkron).
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            #region Dolar Kuru Alma
+            var client = new HttpClient(); // HTTP istekleri için istemci oluştur.
+            var request = new HttpRequestMessage // Yeni bir HTTP isteği tanımla.
+            {
+                Method = HttpMethod.Get, // İstek yöntemi: GET.
+                RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=USD&to=TRY&amount=1"), // API adresi (USD->TRY).
+                Headers = // İstek başlıkları (headers).
+                {
+                    { "x-rapidapi-key", "630ce9cc86msh271c60cffe62d5ep1b514djsn0fe292593744" }, // API anahtarı.
+                    { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" }, // API sunucusu.
+                },
+            };
+            // İsteği gönder ve yanıtı bekle ('using' ile kaynakları yönet).
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode(); // Yanıtın başarılı olduğunu kontrol et (hata varsa exception fırlatır).
+                var body = await response.Content.ReadAsStringAsync(); // Yanıt içeriğini string olarak oku.
+                var json = JObject.Parse(body); // JSON metnini parse et.
+                var value = json["result"].ToString(); // JSON'dan kur değerini ('result' alanı) al.
+                lblDollar.Text = "Dolar: " + value; // Dolar etiketini güncelle.
+                dollar = decimal.Parse(value); // Kur değerini decimal'a çevirip değişkene ata.
+            }
+            #endregion
+
+            #region Euro Kuru Alma
+            var client2 = new HttpClient(); // Yeni HTTP istemcisi (Euro için).
+            var request2 = new HttpRequestMessage // Yeni HTTP isteği (Euro için).
+            {
+                Method = HttpMethod.Get, // İstek yöntemi: GET.
+                RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=EUR&to=TRY&amount=1"), // API adresi (EUR->TRY).
+                Headers = // İstek başlıkları.
+                {
+                    { "x-rapidapi-key", "630ce9cc86msh271c60cffe62d5ep1b514djsn0fe292593744" }, // API anahtarı.
+                    { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" }, // API sunucusu.
+                },
+            };
+            // İsteği gönder ve yanıtı bekle.
+            using (var response2 = await client2.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode(); // Yanıt başarısını kontrol et.
+                var body2 = await response2.Content.ReadAsStringAsync(); // Yanıt içeriğini oku.
+                var json2 = JObject.Parse(body2); // JSON'u parse et.
+                var value2 = json2["result"].ToString(); // Kur değerini al.
+                lblEuro.Text = "Euro: " + value2; // Euro etiketini güncelle.
+                euro = decimal.Parse(value2); // Kuru decimal'a çevirip değişkene ata.
+            }
+            #endregion
+
+            #region Sterlin Kuru Alma
+            var client3 = new HttpClient(); // Yeni HTTP istemcisi (Sterlin için).
+            var request3 = new HttpRequestMessage // Yeni HTTP isteği (Sterlin için).
+            {
+                Method = HttpMethod.Get, // İstek yöntemi: GET.
+                RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=GBP&to=TRY&amount=1"), // API adresi (GBP->TRY).
+                Headers = // İstek başlıkları.
+                {
+                    { "x-rapidapi-key", "630ce9cc86msh271c60cffe62d5ep1b514djsn0fe292593744" }, // API anahtarı.
+                    { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" }, // API sunucusu.
+                },
+            };
+            // İsteği gönder ve yanıtı bekle.
+            using (var response3 = await client3.SendAsync(request3))
+            {
+                response3.EnsureSuccessStatusCode(); // Yanıt başarısını kontrol et.
+                var body3 = await response3.Content.ReadAsStringAsync(); // Yanıt içeriğini oku.
+                var json3 = JObject.Parse(body3); // JSON'u parse et.
+                var value3 = json3["result"].ToString(); // Kur değerini al.
+                lblPound.Text = "Pound: " + value3; // Sterlin etiketini güncelle.
+                pound = decimal.Parse(value3); // Kuru decimal'a çevirip değişkene ata.
+            }
+            #endregion
+
+            txtTotalPrice.Enabled = false; // Toplam tutar metin kutusunu devre dışı bırak (sadece gösterim).
+        }
+
+        // Buton 1'e tıklandığında çalışan olay işleyici.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Birim fiyat metin kutusundaki değeri decimal'a çevir.
+            decimal unitPrice = decimal.Parse(txtUnitPrice.Text);
+
+            // Toplam tutar değişkeni.
+            decimal totalPrice = 0;
+
+            // Seçili olan radio butona göre hesaplama yap.
+            if (rdbDollar.Checked) // Dolar seçiliyse.
+            {
+                totalPrice = unitPrice * dollar; // Toplam = Birim Fiyat * Dolar Kuru.
+            }
+            if (rdbEuro.Checked) // Euro seçiliyse.
+            {
+                totalPrice = unitPrice * euro; // Toplam = Birim Fiyat * Euro Kuru.
+            }
+            if (rdbPound.Checked) // Sterlin seçiliyse.
+            {
+                totalPrice = unitPrice * pound; // Toplam = Birim Fiyat * Sterlin Kuru.
+            }
+
+            // Hesaplanan toplam tutarı metin kutusunda göster.
+            txtTotalPrice.Text = totalPrice.ToString();
+        }
+    }
+}
